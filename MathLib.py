d4d212302546c41112b504d92433b27c875b1e9b
Mazureli <liammazure2001@gmail.com>
2023-02-10 14:43:58 -0500

    Finished Matrix class + Started Vector2 tests

--- MathLib.py
+++ MathLib.py
@@ -46,10 +46,10 @@ class Vector3():
         return Vector3(self.x / magnitude, self.y / magnitude, self.z / magnitude, self.w)
 
     def mag_with(self):
-        return math.sqrt(self.x**2 + self.y**2 + self.z**2 + self.w**2)
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
     def mag_with_out(self):
-        return self.x**2 + self.y**2 + self.z**2 + self.w**2
+        return self.x**2 + self.y**2 + self.z**2
 
     def same_vec(self, other):
         return self.x == other.x and self.y == other.y and self.z == other.z and self.w == other.w
@@ -61,7 +61,9 @@ class Vector2():
         self.w = w
 
     def cross_product(self, other):
-         return self.x * other.y - self.y * other.x
+        x = self.x * other.y
+        y = self.y * other.x
+        return Vector2(x, y,self.w)
 
     def dot_product(self, other):
         return self.x * other.x + self.y * other.y
@@ -71,46 +73,106 @@ class Vector2():
         a_mag = self.mag_with()
         b_mag = self.mag_with_out()
 
-        angle = math.acos(dot / a_mag * b_mag)
+        angle = math.acos(min(1, max(dot / a_mag * b_mag)))
+    
         return angle
 
     def add_vec(self, other):
-        return Vector2(self.x + other.x, self.y + other.y)
+        return Vector2(self.x + other.x, self.y + other.y, self.w)
 
     def sub_vec(self, other):
-        return Vector2(self.x - other.x, self.y - other.y)
+        return Vector2(self.x - other.x, self.y - other.y, self.w)
     
     def normalize(self):
         magnitude = self.mag_with()
         if magnitude == 0:
             return Vector2(0, 0, self.w)
-        return Vector2(self.x / magnitude, self.y / magnitude)
-
+        return Vector2(self.x / magnitude, self.y / magnitude, self.w)
+        
     def mag_with(self):
         return math.sqrt(self.x**2 + self.y**2)
-
+        
     def mag_with_out(self):
         return self.x**2 + self.y**2
 
     def same_vec(self, other):
-        return self.x == other.x and self.y == other.y
+        return self.x == other.x and self.y == other.y and self.w == other.w
 
 class Matrix():
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
+    def __init__(self, data):
+        self.data = data
+        self.rows = len(data)
+        self.cols = len(data[0])
+
+    def shape(self):
+        return(self.rows, self.cols)
     
-    def add_mtx(self):
-        pass
+    def add_mtx(self, other):
+        if self.shape() != other.shape():
+            raise ValueError("Matrcies must be the same size")
+        
+        result = []
 
-    def sub_mtx(self):
-        pass
+        for i in range(self.rows):
+            row = []
+            for j in range(self.cols):
+                row.append(self.data[i][j] + other.data[i][j])
+                result.append(row)
+        
+        return result
 
-    def mult_by_vec(self):
-        pass
+    def sub_mtx(self, other):
+        if self.shape() != other.shape():
+            raise ValueError("Matrcies must be the same size")
+        
+        result = []
 
-    def mult_by_mtx(self):
-        pass
+        for i in range(self.rows):
+            row = []
+            for j in range(self.cols):
+                row.append(self.data[i][j] - other.data[i][j])
+                result.append(row)
+        
+        return result
 
-    def same_mtx(self):
-        pass
+    def mult_by_vec(self, vec):
+        if self.cols != len(vec):
+            raise ValueError("Must have the same number of columns in matrix as elements in the vector")
+        
+        result = []
+
+        for i in range(self.rows):
+            sum = 0
+            for j in range(self.cols):
+                sum += self.data[i][j] * vec[j]
+            result.append(sum)
+
+        return result
+
+    def mult_by_mtx(self,other):
+        if self.shape() != other.shape():
+            raise ValueError("Matrcies must be the same size")
+        
+        result = []
+
+        for i in range(self.rows):
+            row = []
+            for j in range(self.cols):
+                total = 0
+                for k in range(self.cols):
+                    sum += self.data[i][k] * other.data[k][j]
+                row.append(sum)
+            result.append(row)
+        
+        return result
+
+    def same_mtx(self, other):
+        if self.shape() != other.shape():
+            return False
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.data[i][j]  != other.data[i][j]:
+                    return False
+        
+        return True
--- Test.py
+++ Test.py
@@ -1,13 +1,15 @@
 import unittest
 import math
 
+from MathLib import Vector3
 from MathLib import Vector2
+from MathLib import Matrix
 
 
-class TestVector2(unittest.TestCase):
+class TestVector3(unittest.TestCase):
     def test_cross_product(self):
-        v1 = Vector2(1, 2, 3, 0)
-        v2 = Vector2(4, 5, 6, 0)
+        v1 = Vector3(1, 2, 3, 0)
+        v2 = Vector3(4, 5, 6, 0)
         result = v1.cross_product(v2)
 
         self.assertAlmostEqual(-3, result.x)
@@ -15,8 +17,8 @@ class TestVector2(unittest.TestCase):
         self.assertAlmostEqual(-3, result.z)
 
     def test_cross_product_zero_vector(self):
-        v1 = Vector2(0, 0, 0, 0)
-        v2 = Vector2(1, 2, 3, 0)
+        v1 = Vector3(0, 0, 0, 0)
+        v2 = Vector3(1, 2, 3, 0)
         result = v1.cross_product(v2)
 
         self.assertAlmostEqual(0, result.x)
@@ -24,22 +26,22 @@ class TestVector2(unittest.TestCase):
         self.assertAlmostEqual(0, result.z)
 
     def test_dot_product(self):
-        v1 = Vector2(1, 2, 3, 1)
-        v2 = Vector2(4, 5, 6, 1)
+        v1 = Vector3(1, 2, 3, 1)
+        v2 = Vector3(4, 5, 6, 1)
         result = v1.dot_product(v2)
 
         self.assertAlmostEqual(32, result)
 
     def test_dot_product_orthogonal_vectors(self):
-        v1 = Vector2(1, 0, 0, 1)
-        v2 = Vector2(0, 1, 0, 1)
+        v1 = Vector3(1, 0, 0, 1)
+        v2 = Vector3(0, 1, 0, 1)
         result = v1.dot_product(v2)
 
         self.assertAlmostEqual(0, result)
 
     def test_angle_between(self):
-        v1 = Vector2(1, 2, 3, 0)
-        v2 = Vector2(4, 5, 6, 0)
+        v1 = Vector3(1, 2, 3, 0)
+        v2 = Vector3(4, 5, 6, 0)
         result = v1.angle_between(v2)
 
         dot_product = v1.dot_product(v2)
@@ -49,29 +51,29 @@ class TestVector2(unittest.TestCase):
         self.assertAlmostEqual(expected, result)
 
     def test_angle_between_zero_vectors(self):
-        v1 = Vector2(0, 0, 0, 0)
-        v2 = Vector2(0, 0, 0, 0)
+        v1 = Vector3(0, 0, 0, 0)
+        v2 = Vector3(0, 0, 0, 0)
         result = v1.angle_between(v2)
 
         self.assertAlmostEqual(0, result)
 
     def test_angle_between_parallel_vectors(self):
-        v1 = Vector2(1, 2, 3,0)
-        v2 = Vector2(2, 4, 6,0)
+        v1 = Vector3(1, 2, 3,0)
+        v2 = Vector3(2, 4, 6,0)
         result = v1.angle_between(v2)
 
         self.assertAlmostEqual(0, result)
 
     def test_angle_between_orthogonal_vectors(self):
-        v1 = Vector2(1, 0, 0, 0)
-        v2 = Vector2(0, 1, 0, 0)
+        v1 = Vector3(1, 0, 0, 0)
+        v2 = Vector3(0, 1, 0, 0)
         result = v1.angle_between(v2)
 
         self.assertAlmostEqual(math.pi / 2, result)
 
     def test_add_vec(self):
-        v1 = Vector2(1, 2, 3, 0)
-        v2 = Vector2(4, 5, 6, 0)
+        v1 = Vector3(1, 2, 3, 0)
+        v2 = Vector3(4, 5, 6, 0)
         result = v1.add_vec(v2)
 
         self.assertEqual(5, result.x)
@@ -80,8 +82,8 @@ class TestVector2(unittest.TestCase):
         self.assertEqual(0, result.w)
 
     def test_sub_vec(self):
-        v1 = Vector2(1, 2, 3, 0)
-        v2 = Vector2(4, 5, 6, 0)
+        v1 = Vector3(1, 2, 3, 0)
+        v2 = Vector3(4, 5, 6, 0)
         result = v1.sub_vec(v2)
 
         self.assertEqual(-3, result.x)
@@ -90,7 +92,7 @@ class TestVector2(unittest.TestCase):
         self.assertEqual(0, result.w)
 
     def test_normalize(self):
-        v = Vector2(3, 4, 5, 0)
+        v = Vector3(3, 4, 5, 0)
         result = v.normalize()
         self.assertAlmostEqual(1, result.mag_with())
         self.assertAlmostEqual(3 / math.sqrt(50), result.x)
@@ -98,14 +100,115 @@ class TestVector2(unittest.TestCase):
         self.assertAlmostEqual(5 / math.sqrt(50), result.z)
 
     def test_magnitude(self):
-        v = Vector2(1,2,3,0)
+        v = Vector3(1,2,3,0)
         result = v.mag_with_out()
         self.assertAlmostEqual(14, result)
 
     def test_same_vec(self):
-        v1 = Vector2(1, 2, 3, 0)
-        v2 = Vector2(1, 2, 3, 0)
-        v3 = Vector2(4, 5, 6, 0)
+        v1 = Vector3(1, 2, 3, 0)
+        v2 = Vector3(1, 2, 3, 0)
+        v3 = Vector3(4, 5, 6, 0)
+        self.assertTrue(v1.same_vec(v2))
+        self.assertFalse(v1.same_vec(v3))
+
+#Vestor2 Tests
+class TestVector2(unittest.TestCase):
+    def test_cross_product(self):
+        v1 = Vector2(1, 2, 0)
+        v2 = Vector2(4, 5, 0)
+        result = v1.cross_product(v2)
+
+        self.assertAlmostEqual(5, result.x)
+        self.assertAlmostEqual(8, result.y)
+
+    def test_cross_product_zero_vector(self):
+        v1 = Vector2(0, 0, 0)
+        v2 = Vector2(1, 2, 0)
+        result = v1.cross_product(v2)
+
+        self.assertAlmostEqual(0, result.x)
+        self.assertAlmostEqual(0, result.y)
+
+    def test_dot_product(self):
+        v1 = Vector2(1, 2, 1)
+        v2 = Vector2(3, 4, 1)
+        result = v1.dot_product(v2)
+
+        self.assertAlmostEqual(11, result)
+
+    def test_dot_product_orthogonal_vectors(self):
+        v1 = Vector2(1, 0, 1)
+        v2 = Vector2(0, 1, 1)
+        result = v1.dot_product(v2)
+
+        self.assertAlmostEqual(0, result)
+
+    def test_angle_between(self):
+        v1 = Vector3(1, 2, 0)
+        v2 = Vector3(3, 4, 0)
+        result = v1.angle_between(v2)
+
+        dot_product = v1.dot_product(v2)
+        magnitude_product = v1.mag_with() * v2.mag_with()
+        expected = math.acos(min(1, max(-1, dot_product / magnitude_product)))
+
+        self.assertAlmostEqual(expected, result)
+
+    def test_angle_between_zero_vectors(self):
+        v1 = Vector2(0, 0, 0)
+        v2 = Vector2(0, 0, 0)
+        result = v1.angle_between(v2)
+
+        self.assertAlmostEqual(0, result)
+
+    def test_angle_between_parallel_vectors(self):
+        v1 = Vector2(1, 2,0)
+        v2 = Vector2(3, 4,0)
+        result = v1.angle_between(v2)
+
+        self.assertAlmostEqual(0, result)
+
+    def test_angle_between_orthogonal_vectors(self):
+        v1 = Vector2(1, 0, 0)
+        v2 = Vector2(0, 1, 0)
+        result = v1.angle_between(v2)
+
+        self.assertAlmostEqual(math.pi / 2, result)
+
+    def test_add_vec(self):
+        v1 = Vector2(1, 2, 0)
+        v2 = Vector2(4, 5, 0)
+        result = v1.add_vec(v2)
+
+        self.assertEqual(5, result.x)
+        self.assertEqual(7, result.y)
+        self.assertEqual(0, result.w)
+
+    def test_sub_vec(self):
+        v1 = Vector2(1, 2, 0)
+        v2 = Vector2(4, 5, 0)
+        result = v1.sub_vec(v2)
+
+        self.assertEqual(-3, result.x)
+        self.assertEqual(-3, result.y)
+        self.assertEqual(0, result.w)
+
+    def test_normalize(self):
+        v = Vector2(3, 4, 0)
+        result = v.normalize()
+        self.assertAlmostEqual(1, result.mag_with())
+        self.assertAlmostEqual(3 / math.sqrt(50), result.x)
+        self.assertAlmostEqual(4 / math.sqrt(50), result.y)
+
+    def test_magnitude(self):
+        v = Vector2(1,2,0)
+        result = v.mag_with_out()
+        self.assertAlmostEqual(5, result)
+
+    def test_same_vec(self):
+        v1 = Vector2(1, 2, 0)
+        v2 = Vector2(1, 2, 0)
+        v3 = Vector2(4, 5, 0)
         self.assertTrue(v1.same_vec(v2))
         self.assertFalse(v1.same_vec(v3))
 
